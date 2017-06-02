4ef133675b0820bd3c209e157d50eb625b4e4944
Benjamin Bucca <benjamin.bucca3@gmail.com>
2017-06-02 11:40:13 -0700

    handling user events

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-// TODO
+var TodoList = (props) => {
+
+  // This function will be called when the first `<li>` below is clicked on
+  // Notice that event handling functions receive an `event` object
+  // We want to define it where it has access to `props`
+
+  var onListItemClick = (event) => {
+    console.log('I got clicked');
+  };
+
+  // Because we used curly braces with this arrow function
+  // we have to write an explicit `return` statement
+  return (
+    <ul>
+      <li onClick={onListItemClick}>{props.todos[0]}</li>
+      <li>{props.todos[1]}</li>
+      <li>{props.todos[2]}</li>
+    </ul>
+  );
+}
+
+var App = () => (
+  <div>
+    <h2>My Todo List</h2>
+    <TodoList todos={['Learn React', 'Crush Recast.ly', 'Maybe sleep']}/>
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById("app"));
